babdd17f6c67375deea520a8994baa895d34e893
Jordan Ellis <Jordieellis@outlook.com>
2025-07-15 18:14:39 +1200

    feat: need to be able to accept multiple records not just one

--- src/models/records.model.ts
+++ src/models/records.model.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
-export const RecordSchema = z.object({
+const RecordSchema = z.object({
   total: z.number(),
   recordType: z.enum(["invoice", "bill", "none"]),
   status: z.enum(["pending", "void", "completed"]),
 });
+
+export const RecordsSchema = z.array(RecordSchema);
+export type Record = z.infer<typeof RecordSchema>;
+export type Records = z.infer<typeof RecordSchema>;
