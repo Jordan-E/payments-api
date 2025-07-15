74f268fbb9c4cafc0f5ded7da67cd9331c34b0f5
Jordan Ellis <Jordieellis@outlook.com>
2025-07-15 19:12:00 +1200

    feat: new tests to check for records being returned

--- test/records.test.ts
+++ test/records.test.ts
@@ -1,6 +1,13 @@
 import { db } from "../src/db/database";
 import { app } from "../src/index";
 import request from "supertest";
+import { Record } from "../src/models/records.model";
+
+const defaultRecord: Record = {
+  total: 100,
+  recordType: "bill", // Ensure the type matches the expected values
+  status: "pending",
+};
 
 beforeAll(async () => {
   await db.deleteFrom("payments").execute();
@@ -14,6 +21,22 @@ describe("Getting records", () => {
   it("Get no data", async () => {
     const res = await request(app).get("/records").query({});
     expect(res.statusCode).toBe(200);
+  });
+
+  it("Manually added row", async () => {
+    db.insertInto("payments")
+      .values({
+        total: defaultRecord.total,
+        record_type: defaultRecord.recordType,
+        status: defaultRecord.status,
+        create_date: new Date().toISOString(),
+        modified_date: new Date().toISOString(),
+      })
+      .execute();
+
+    const res = await request(app).get("/records").query({});
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveLength(1);
   });
 });
 describe("Get records with filter", () => {});
